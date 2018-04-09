fdf5f7381d60c6ce6061841a16685578ca794369
Satya RC <satyarchandra@gmail.com>
2018-04-09 16:45:11 +0530

    Fixed duplicate injection

--- src/app/pcodes/pcodes.component.ts
+++ src/app/pcodes/pcodes.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Component, OnInit } from '@angular/core';
 import { HttpClient, HttpParams, HttpRequest} from '@angular/common/http';
 import { HttpHeaders } from '@angular/common/http';
 import 'rxjs/Rx';
